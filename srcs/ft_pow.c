6105662dce28f83afb23b054542841b04111e437
Arnell Norjen <anorjen@e2r3p1.21-school.ru>
2018-12-24 19:18:22 +0300

    add fix dtoa, stoa, pow

--- srcs/ft_pow.c
+++ srcs/ft_pow.c
@@ -1,30 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_dtoa.c                                          :+:      :+:    :+:   */
+/*   ft_pow.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anorjen <anorjen@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/24 13:19:27 by anorjen           #+#    #+#             */
-/*   Updated: 2018/12/24 18:54:19 by anorjen          ###   ########.fr       */
+/*   Created: 2018/12/24 14:31:57 by anorjen           #+#    #+#             */
+/*   Updated: 2018/12/24 18:36:03 by anorjen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-char	*ft_dtoa(long double num, int acc)
+long double	ft_pow(long double num, int power)
 {
-	ssize_t		whole;
-	ssize_t		fraction;
-	char		*res;
-	char		*buf;
-	long double	eps;
-
-	eps = 0.5 / ft_pow(10, acc);
-	whole = (ssize_t)num;
-	fraction = (size_t)(ABS(num - (long double)whole + eps) * ft_pow(10.0, acc));
-	buf = ft_strjoin(ft_stoa(whole), ".");
-	res = ft_strjoin(buf, ft_stoa(fraction));
-	free(buf);
-	return (res);
+	if (power == 0)
+		return (1);
+	return(num * ft_pow(num, power - 1));
 }
