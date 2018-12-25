c311a5574c423ea309f58f204c59e482412adb82
Rupert brax Schuppe <rschuppe@e2r2p4.21-school.ru>
2018-12-25 19:14:14 +0300

    update spec hex & p & s

--- srcs/printf/spec_hex.c
+++ srcs/printf/spec_hex.c
@@ -6,7 +6,7 @@
 /*   By: rschuppe <rschuppe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/23 17:25:02 by rschuppe          #+#    #+#             */
-/*   Updated: 2018/12/25 17:54:22 by rschuppe         ###   ########.fr       */
+/*   Updated: 2018/12/25 19:11:01 by rschuppe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,45 +14,74 @@
 
 int	print_hex(void *value, t_spec spec)
 {
-	char *res;
+	char	*res;
+	int		len;
 
 	res = NULL;
 	if (spec.size == SIZE_l)
-	{
 		res = ft_itoa_base(*((long*)value), 16);
+	else
+		res = ft_itoa_base(*((int*)value), 16);
+	len = ft_strlen(res);
+	if (spec.width > 1 && spec.width - len > 0)
+	{
+		if (spec.flags & FLAG_MINUS)
+		{
+			if (spec.flags & FLAG_OCTOP)
+				ft_putstr("0X");
+			ft_putstr(res);
+			ft_printchr(spec.width - len, ' ');
+		}
+		else
+		{
+			ft_printchr(spec.width - len, ' ');
+			if (spec.flags & FLAG_OCTOP)
+				ft_putstr("0X");
+			ft_putstr(res);
+		}
 	}
 	else
 	{
-		res = ft_itoa_base(*((int*)value), 16);
+		if (spec.flags & FLAG_OCTOP)
+			ft_putstr("0X");
+		ft_putstr(res);
 	}
-	ft_putstr(res);
-	return (ft_strlen(res));
+	return (len > spec.width ? len : spec.width);
 }
 
 int	print_hex_lower(void *value, t_spec spec)
 {
-	char *res;
+	char	*res;
+	int		len;
 
 	res = NULL;
 	if (spec.size == SIZE_l)
-	{
 		res = ft_strlower(ft_itoa_base(*((long*)value), 16));
+	else
+		res = ft_strlower(ft_itoa_base(*((int*)value), 16));
+	len = ft_strlen(res);
+	if (spec.width > 1 && spec.width - len > 0)
+	{
+		if (spec.flags & FLAG_MINUS)
+		{
+			if (spec.flags & FLAG_OCTOP)
+				ft_putstr("0x");
+			ft_putstr(res);
+			ft_printchr(spec.width - len, ' ');
+		}
+		else
+		{
+			ft_printchr(spec.width - len, ' ');
+			if (spec.flags & FLAG_OCTOP)
+				ft_putstr("0x");
+			ft_putstr(res);
+		}
 	}
 	else
 	{
-		res = ft_strlower(ft_itoa_base(*((int*)value), 16));
+		if (spec.flags & FLAG_OCTOP)
+			ft_putstr("0x");
+		ft_putstr(res);
 	}
-	// if (spec.width > 1)
-	// {
-	// 	if (spec.flags & FLAG_MINUS)
-	// 	{
-
-	// 	}
-	// 	else
-	// 	{
-
-	// 	}
-	// }
-	ft_putstr(res);
-	return (ft_strlen(res));
+	return (len > spec.width ? len : spec.width);
 }
--- srcs/printf/spec_s.c
+++ srcs/printf/spec_s.c
@@ -6,7 +6,7 @@
 /*   By: rschuppe <rschuppe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/23 17:25:25 by rschuppe          #+#    #+#             */
-/*   Updated: 2018/12/25 17:11:49 by rschuppe         ###   ########.fr       */
+/*   Updated: 2018/12/25 19:09:18 by rschuppe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,23 @@
 
 int	print_string(void *value, t_spec spec)
 {
-	ft_putstr((char*)value);
-	return (ft_strlen(value));
+	int len;
+
+	len = ft_strlen(value);
+	if (spec.width > 1 && spec.width - len > 0)
+	{
+		if (spec.flags & FLAG_MINUS)
+		{
+			ft_putstr((char*)value);
+			ft_printchr(spec.width - len, ' ');
+		}
+		else
+		{
+			ft_printchr(spec.width - len, ' ');
+			ft_putstr((char*)value);
+		}
+	}
+	else
+		ft_putstr((char*)value);
+	return (len > spec.width ? len : spec.width);
 }
